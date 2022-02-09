cc850352ed7fb6cd9ac571e73897d87fd5112fe5
Tarric Sookdeo <tarricsookdeo@outlook.com>
2022-02-09 12:03:36 -0500

    Add colors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,125 @@
 const App = () => {
   return (
-    <div className='App'>
-      <h1 className='text-3xl'>Test Text</h1>
+    <div className='App h-screen'>
+      <div className='p-20'>
+        <h1 className='text-3xl font-bold text-dwolla-gray-9'>
+          Primary Colors
+        </h1>
+        <div className='flex text-xl font-bold mt-10'>
+          <div>
+            <h2 className='text-dwolla-gray-9 mb-2'>Purple</h2>
+            <div className='w-36 h-36 bg-dwolla-primary-purple'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>Orange</h2>
+            <div className='w-36 h-36 bg-dwolla-primary-orange'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>Gray</h2>
+            <div className='w-36 h-36 bg-dwolla-primary-gray'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>Pink</h2>
+            <div className='w-36 h-36 bg-dwolla-primary-pink'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>Teal</h2>
+            <div className='w-36 h-36 bg-dwolla-primary-teal'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>Yellow</h2>
+            <div className='w-36 h-36 bg-dwolla-primary-yellow'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>Icon Orange</h2>
+            <div className='w-36 h-36 bg-dwolla-primary-iconOrange'></div>
+          </div>
+        </div>
+      </div>
+
+      <div className='pl-20 pb-20'>
+        <h1 className='text-3xl font-bold text-dwolla-gray-9'>Gray</h1>
+        <div className='flex text-xl font-bold mt-10'>
+          <div>
+            <h2 className='text-dwolla-gray-9 mb-2'>1</h2>
+            <div className='w-36 h-36 bg-dwolla-gray-1'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>2</h2>
+            <div className='w-36 h-36 bg-dwolla-gray-2'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>3</h2>
+            <div className='w-36 h-36 bg-dwolla-gray-3'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>4</h2>
+            <div className='w-36 h-36 bg-dwolla-gray-4'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>5</h2>
+            <div className='w-36 h-36 bg-dwolla-gray-5'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>6</h2>
+            <div className='w-36 h-36 bg-dwolla-gray-6'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>7</h2>
+            <div className='w-36 h-36 bg-dwolla-gray-7'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>8</h2>
+            <div className='w-36 h-36 bg-dwolla-gray-8'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>9</h2>
+            <div className='w-36 h-36 bg-dwolla-gray-9'></div>
+          </div>
+        </div>
+      </div>
+
+      <div className='pl-20'>
+        <h1 className='text-3xl font-bold text-dwolla-gray-9'>Dark Purple</h1>
+        <div className='flex text-xl font-bold mt-10'>
+          <div>
+            <h2 className='text-dwolla-gray-9 mb-2'>1</h2>
+            <div className='w-36 h-36 bg-dwolla-darkPurple-1'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>2</h2>
+            <div className='w-36 h-36 bg-dwolla-darkPurple-2'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>3</h2>
+            <div className='w-36 h-36 bg-dwolla-darkPurple-3'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>4</h2>
+            <div className='w-36 h-36 bg-dwolla-darkPurple-4'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>5</h2>
+            <div className='w-36 h-36 bg-dwolla-darkPurple-5'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>6</h2>
+            <div className='w-36 h-36 bg-dwolla-darkPurple-6'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>7</h2>
+            <div className='w-36 h-36 bg-dwolla-darkPurple-7'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>8</h2>
+            <div className='w-36 h-36 bg-dwolla-darkPurple-8'></div>
+          </div>
+          <div className='ml-10'>
+            <h2 className='text-dwolla-gray-9 mb-2'>9</h2>
+            <div className='w-36 h-36 bg-dwolla-darkPurple-9'></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,43 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        dwolla: {
+          primary: {
+            purple: '#2D2D48',
+            orange: '#E37C53',
+            gray: '#8393AA',
+            pink: '#BE5771',
+            teal: '#4FA4A5',
+            yellow: '#F79D52',
+            iconOrange: '#CA4C2F'
+          },
+          gray: {
+            1: '#F4F7FB',
+            2: '#EAEDF3',
+            3: '#C9D3E0',
+            4: '#AAB7C8',
+            5: '#8393AA',
+            6: '#52627B',
+            7: '#3E4E67',
+            8: '#354153',
+            9: '#2E3745'
+          },
+          darkPurple: {
+            1: '#D5D5DA',
+            2: '#ABABB6',
+            3: '#818191',
+            4: '#57576D',
+            5: '#4A4A5E',
+            6: '#424259',
+            7: '#3A3A51',
+            8: '#34344F',
+            9: '#2D2D48'
+          }
+        }
+      }
+    }
   },
   plugins: []
 };
